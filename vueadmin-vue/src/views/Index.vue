cc9e387113b9d2fe46a050bb3f23181f4fe6540b
icatw <762188827@qq.com>
2022-04-30 00:13:41 +0800

    完成头部开发

--- vueadmin-vue/src/views/Index.vue
+++ vueadmin-vue/src/views/Index.vue
@@ -1,5 +1,30 @@
 <template>
-<h1>首页</h1>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+
+      <el-header>
+        <strong>VueAdmin后台管理系统</strong>
+        <div class="header-avatar">
+          <el-avatar size="medium"
+                     src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+          <el-dropdown>
+            <span class="el-dropdown-link">
+            Admin<i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>个人中心</el-dropdown-item>
+              <el-dropdown-item>退出</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <el-link href="http://www.icatw.top" target="_blank">icatwの博客</el-link>
+          <el-link href="https://gitee.com/icatw" target="_blank">Gitee</el-link>
+        </div>
+      </el-header>
+
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -9,5 +34,60 @@ export default {
 </script>
 
 <style scoped>
+.header-avatar {
+  float: right;
+  width: 250px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
 
+.el-container {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+}
+
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409EFF;
+}
+
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
 </style>
